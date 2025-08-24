3bdd73fc0ccf50f212492b002ca2c2a0987851c1
Dev093235 <dev431140@gmail.com>
2025-08-24 13:53:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+
+// Render ke liye PORT environment variable ka use
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// Test Route
 app.get('/', (req, res) => {
   res.send('Online Class Backend Running...');
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
