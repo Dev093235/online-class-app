c879b3fe90cd1591a5411478cf1a50ea6b40aff2
Dev093235 <dev431140@gmail.com>
2025-08-24 14:03:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// Auth Routes import
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 // Test Route
 app.get('/', (req, res) => {
   res.send('Online Class Backend Running...');
