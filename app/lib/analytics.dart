752aeafe499992aa74c0b4884fcf904daf4ac58f
Dev093235 <dev431140@gmail.com>
2025-08-25 20:34:42 +0530

    Create analytics.dart

---
+++ app/lib/analytics.dart
@@ -0,0 +1,11 @@
+class AnalyticsService {
+  void trackEvent(String eventName) {
+    // Dummy analytics logic
+    print("Tracked event: $eventName");
+  }
+
+  int getTotalViews() {
+    // Dummy data
+    return 100;
+  }
+}
