01467fbdac01071d0e44b96d698f9e60eef1b768
Dev093235 <dev431140@gmail.com>
2025-08-25 22:52:16 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'navigation.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Online Class App',
+      theme: AppTheme.getLightTheme(),
+      home: Navigation(),
+    );
+  }
