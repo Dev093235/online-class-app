c999e102e42129cf99ecb4cf506d2df462522278
Dev093235 <dev431140@gmail.com>
2025-08-26 16:53:24 +0530

    Delete app directory

--- app/lib/config.dart
+++
@@ -1,4 +0,0 @@
-class AppConfig {
-  static const String appName = "Online Class App";
-  static const String baseUrl = "https://example.com/api";
-}
--- app/lib/screens.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'main.dart';
-import 'second.dart';
-
-class ScreenManager {
-  static Widget getHomeScreen() => MyApp();
-  static Widget getSecondScreen() => SecondScreen();
-}
